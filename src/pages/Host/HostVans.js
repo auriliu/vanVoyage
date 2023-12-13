27b8a0b3a5fc9c474e655e019e72f382c6ee3867
audesmus <aureliu.aurelius@gmail.com>
2023-12-13 22:13:41 +0100

    host vans fetching data from miragejs server.

--- src/pages/Host/HostVans.js
+++ src/pages/Host/HostVans.js
@@ -8,34 +8,34 @@ function HostVans() {
   useEffect(() => {
     fetch("/api/host/vans")
       .then((res) => res.json())
-      .then((data) => setVans(data));
+      .then((data) => setVans(data.vans));
   }, []);
 
-  vans.length > 0 ? (
-    vans.map((van) => (
-      <Link
-        to={`/host/vans/${van.id}`}
-        key={van.id}
-        className="host-van-link-wrapper"
-      >
-        <div className="host-van-single">
-          <img src={van.imageUrl} alt="" />
-          <div className="host-van-info">
-            <h3>{van.name}</h3>
-            <p>${van.price}/day</p>
-          </div>
+  const hostVansEls = vans.map((van) => (
+    <Link
+      to={`/host/vans/${van.id}`}
+      key={van.id}
+      className="host-van-link-wrapper"
+    >
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt="" />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
         </div>
-      </Link>
-    ))
-  ) : (
-    <p>loading still...</p>
-  );
+      </div>
+    </Link>
+  ));
 
   return (
     <section>
       <h1 className="host-vans-title">your listed vans</h1>
       <div className="host-vans-list">
-        {vans.length > 0 ? <section> {vans}</section> : <p>loading...</p>}
+        {vans.length > 0 ? (
+          <section>{hostVansEls}</section>
+        ) : (
+          <p>loading host vans...</p>
+        )}
       </div>
     </section>
   );
